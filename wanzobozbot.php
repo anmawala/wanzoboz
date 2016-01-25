3185b025f70be2741c2029b3f4a43d5b4035a558
wanzola <antonio.laviola@gmail.com>
2016-01-25 23:49:34 +0100

    Base Definitiva - Si comincia da qui
    Ho testato sul mio server questa base e funziona. Rimane il lato server
    sa sistemare, ma solo tu sai.

--- wanzobozbot.php
+++ wanzobozbot.php
@@ -38,6 +38,7 @@ function exec_curl_request($handle) {
 
   return $response;
 }
+
 function apiRequest($method, $parameters) {
   if (!is_string($method)) {
     error_log("Method name must be a string\n");
@@ -66,150 +67,34 @@ function apiRequest($method, $parameters) {
 
   return exec_curl_request($handle);
 }
-function avvisi($chat_id) {
-  $finale = "Questi sono gli <b>ultimi 5 avvisi</b> presenti su Campusnet:\n";
-  $content = file_get_contents("http://medtriennaliasl4.campusnet.unito.it/do/avvisi.pl/Search");
-  preg_match_all('/(?:hits=[\d]*">)(.*?)(?:<)/',$content,$avvisi);
-  preg_match_all('/(?:A HREF=")(.*?)(?:;sort)/',$content,$links);
-  for ($i=0; $i < 5 ; $i++) {
-    $link = "http://medtriennaliasl4.campusnet.unito.it".$links[1][$i];
-    $avviso = utf8_encode($avvisi[1][$i]);
-    $finale .= "\n".($i+1)."."."\t"."<a href=\"$link\">$avviso</a>";
-  }
-  apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => $finale, "parse_mode" => "HTML"));
-  return $finale;
-}
-function appelli($chat_id) {
-  $finale = "Queste sono le <b>ultime 5 ADE</b> aggiunte su Campusnet:\n";
-  $finale_inline = "<b>ultime 5 ADE</b> aggiunte:\n";
-  $content = file_get_contents("http://medtriennaliasl4.campusnet.unito.it/do/appelli.pl/Search?since=&days=10&sort=TIME&format=&cookie=NEWS");
-  preg_match_all('/(?:hits=[\d]*"><B>)(.*?)(?:<\/B>)/',$content,$appelli);
-  preg_match_all('/(?:px">Luogo: )(.*?)(?:<\/div><\/TD>)/',$content,$sedi);
-  preg_match_all('/(?:TOP><A HREF=")(.*?)(?:;sort)/',$content,$links);
-  preg_match_all('/(?:OWRAP>)(.*?)(?:<BR>)/',$content,$date);
-  preg_match_all('/(?:ore<\/I>:)(.*?)(?:<BR>)/',$content,$orari);
-  for ($i=0; $i < 5 ; $i++) {
-    $link = 'http://medtriennaliasl4.campusnet.unito.it'.$links[1][$i];
-    $appello = utf8_encode($appelli[1][$i]);
-    $appello = str_replace("a.a. 15/16 - ", "", $appello);
-    $sede = utf8_encode($sedi[1][$i]);
-    if(preg_match('/HREF/', $sede)==1) {
-      preg_match_all('/(?:>)(.*?)(?:<)/', $sede, $sede);
-      $sede = utf8_encode($sede[1][0]);
-    }
-    $data = $date[1][$i];
-    $data = strip_tags($data);
-    $orario = $orari[1][$i];
-    $finale .= "\n".($i+1)."."."\t"."<a href=\"$link\">$appello</a>"."\n"."Orario: ".$orario."\t\t"."Data: ".$data."\n"."Sede: ".$sede."\n\n";
-    $finale_inline .= ($i+1)."."."\t"."$appello"."\n";
-  }
-  apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => $finale, "parse_mode" => "HTML"));
-  return $finale_inline;
-}
-function elencoComandi($chat_id, $comandi) {
-  $elenco = "I comandi attualmente disponibili (non case sensitive) sono:\n
--\t*$comandi[0]*\t_Mostra gli ultimi 5 avvisi di Campusnet_
--\t*$comandi[5]*\t_Mostra le ultime 5 ADE aggiunte su Campusnet_
--\t*$comandi[1]*\t_relativa al 2015/2016_
--\t*$comandi[2]*\t_relativa al 2015/2016_
--\t*$comandi[3]*\t_Invia gli orari di lezione dei 3 anni (I semestre)_
--\t*$comandi[6]*\t_Invia il calendario esami da Febbraio a Dicembre 2016 DM270/04_
--\t*$comandi[4]*\t_Apre una tastiera con i comandi (_ *Chiudi*_ per chiuderla)_
 
-  /help\t_Riepiloga i comandi disponibili_
-  ";
-  apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => $elenco, "parse_mode" => "Markdown"));
-}
-function tastieraComandi($chat_id, $comandi) {
-  apiRequest("sendMessage", array("chat_id" => $chat_id, "text" => "Ecco la tastierina dei comandi", "reply_markup" => array("keyboard" => array(array("Avvisi", "ADE"), array("Orari", "Piano studi"), array("Calendario esami"), array("Programmazione didattica"), array("/help")),"resize_keyboard" => true, "one_time_keyboard" => false)));
-}
 function processMessage($message) {
   // process incoming message
-  $comandi = array("Avvisi", "Programmazione didattica", "Piano studi", "Orari", "Tastiera", "ADE", "Calendario esami");
   $message_id = $message['message_id'];
   $chat_id = $message['chat']['id'];
-  if (isset($message['text']))
-  {
+
+  if (isset($message['text'])) {
     // incoming text message
     $text = $message['text'];
-    if($chat_id!=22699108) apiRequest("sendMessage", array('chat_id' => 22699108, "text" => $message["from"]["first_name"]." ".$message["from"]["last_name"]." @".$message["from"]["username"]." ".$text));
-    if (preg_match('/[A,a]vvisi/',$text)==1) {
-      avvisi($chat_id);
-    }
-    else if (preg_match('/ADE/',$text)==1) {
-      appelli($chat_id);
-    }
-    else if (preg_match('/\/start/',$text)==1) {
-      apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "Ciao ".$message["from"]["first_name"].". Benvenuto/a nel BOT (non ufficiale) del Corso di Laurea di Infermieristica To2", "parse_mode" => "Markdown"));
-      elencoComandi($chat_id, $comandi);
-      apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "_Digita o premi e invia al BOT il comando desiderato_","parse_mode" => "Markdown"));
-      tastieraComandi($chat_id, $comandi);
-    }
-    else if (preg_match('/\/help/',$text)==1) {
-      elencoComandi($chat_id, $comandi);
-    }
-    else if (preg_match('/[P,p]rogrammazione [D,d]idattica/',$text)==1) {
-      apiRequest("sendDocument", array('chat_id' => $chat_id, "document" => "BQADAgADcQMAAmRcWgEwELLnffwxAAEC"));
-    }
-    else if (preg_match('/[P,p]iano [S,s]tudi/',$text)==1) {
-      apiRequest("sendDocument", array('chat_id' => $chat_id, "document" => "BQADAgADcgMAAmRcWgEcV4sMywABpd8C"));
-    }
-    else if (preg_match('/[C,c]alendario [E,e]sami/',$text)==1) {
-      apiRequest("sendDocument", array('chat_id' => $chat_id, "document" => "BQADAgADkwMAAmRcWgGAGHRrXWC4BQI"));
-    }
-    else if (preg_match('/[O,o]rari/',$text)==1) {
-      apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "1° anno I semestre"));
-      apiRequest("sendDocument", array('chat_id' => $chat_id, "document" => "BQADAgADcwMAAmRcWgEjPbNmeZg69wI"));
-      apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "2° anno I semestre"));
-      apiRequest("sendDocument", array('chat_id' => $chat_id, "document" => "BQADAgADdAMAAmRcWgGfmubLVh-RlAI"));
-      apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "3° anno I semestre"));
-      apiRequest("sendDocument", array('chat_id' => $chat_id, "document" => "BQADAgADdQMAAmRcWgFpF1HAl582aAI"));
-    }
-    else if (preg_match('/[T,t]astiera/',$text)==1) {
-      tastieraComandi($chat_id, $comandi);
-    }
-    else if (preg_match('/[C,c]hiudi/',$text)==1) {
-      apiRequest("sendMessage", array("chat_id" => $chat_id, "text" => "Chiusura tastiera", "reply_markup" => array("hide_keyboard" => true)));
-      }
-    else if (preg_match('/data/',$text)==1) {
-        apiRequest("sendMessage", array("chat_id" => array(22699108,22699108), "text" => "CIAO"));
+    if (preg_match('/\/start/',$text)==1) {
+      apiRequest("sendMessage", array('chat_id' => $chat_id, 'text' => "WanzoBozBot è vivo!"));
     }
     else {
-      apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "Mi spiace, nessun azione disponibile con questo comando."));
+      apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "Bio Parco!"));
     }
   }
-  else {
-    apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Mi spiace, leggo solo i messaggi di testo'));
-  }
 }
+
 
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 
+if (!$update) {
+  // receive wrong update, must not happen
+  exit;
+}
+
 if (isset($update["message"])) {
   processMessage($update["message"]);
 }
-
-if (isset($update["message"]["document"])) {
- $file_id = $update["message"]["document"]["file_id"];
- apiRequest("sendMessage", array('chat_id' => 22699108, "text" => $file_id));
-}
-
-if (isset($update["inline_query"]["query"])) {
-  $id = $update["inline_query"]["id"];
-  $query = $update["inline_query"]["query"];
-  $chat_id = $update["inline_query"]["from"]["id"];
-  if ($query=="avvisi") {
-    $avvisi = avvisi("0");
-    apiRequest('answerInlineQuery', array('inline_query_id' => $id, 'results' => array(array("type" => "article","id" => "0","title" => "Mostra gli ultimi 5 avvisi pubblicati su Campusnet","message_text" => $avvisi, "parse_mode" => "HTML", "cache_time" => 0))));
-  }
-  else if ($query=="ade") {
-    $ade = appelli("0");
-    apiRequest('answerInlineQuery', array('inline_query_id' => $id, 'results' => array(array("type" => "article","id" => "0","title" => "Mostra le ultime 5 ADE pubblicate su Campusnet","message_text" => $ade, "parse_mode" => "HTML", "cache_time" => 0))));
-  }
-  else if ($query=="presenta") {
-    apiRequest('answerInlineQuery', array('inline_query_id' => $id, 'results' => array(array("type" => "article","id" => "0","title" => "Presenta il bot","message_text" => "*CLITo2Bot* è il bot, creato da uno studente in via non ufficiale, che permette di utilizzare alcuni strumenti utili come la lettura di avvisi o la consultazione di documenti didattici presenti su Campusnet. Per iniziare ad utilizzare questo Bot, premi [QUI](telegram.me/CLITo2Bot?start=link).", "parse_mode" => "Markdown", "cache_time" => 0))));
-  }
-}
-
 ?>
